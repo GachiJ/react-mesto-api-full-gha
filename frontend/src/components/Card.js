d3b3c862d1cf92d8bcd0d0583bf3388aad713441
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-07-02 17:43:41 +0300

    тест

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -20,7 +20,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete, }) {
   const isOwn = card.owner === currentUser._id;
 
 
-  const isLiked = card.likes.some(i => i === currentUser._id);
+  const isLiked = card.likes.some(like => like === currentUser._id);
   const cardLikeButtonClassName = ( 
     `card__like-button ${isLiked && 'card__like-button_type_active'}` 
   );; 
