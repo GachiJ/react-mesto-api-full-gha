a8430a005bf3704a9d7a09508f1ecbf7bbd986cb
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-07-03 18:00:30 +0300

    тест

--- frontend/src/utilits/AuthApi.js
+++ frontend/src/utilits/AuthApi.js
@@ -42,7 +42,7 @@ class AuthApi {
       credentials: 'include',
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${Cookies.get('jwt')}`
+        "Authorization": `Bearer ${Cookies.get('token')}`
       },
     })
       .then(res => this._checkResponse(res));
