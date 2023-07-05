30d68a25df3c9bacae4a44e527709d9c5e17709b
Nikita Kirsanov <NikitaKirsanov126@yandex.ru>
2023-07-05 10:18:17 +0300

    реализовал настройку и деплой бэкенда

--- backend/app.js
+++ backend/app.js
@@ -14,13 +14,13 @@ app.use(helmet());
 
 const { PORT = 3000, DB_URL = 'mongodb://0.0.0.0:27017/mestodb' } = process.env;
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
 
 app.use(cors({
   credentials: true,
-  origin: DB_URL,
+  origin: 'https://mestohostfront.nomoreparties.sbs',
 }));
 
 app.use(bodyParser.json());
